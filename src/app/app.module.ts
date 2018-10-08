e428fd22deb33aec344cb1e90789caebecb1b227
Technokrat <technokrat.cracker@gmail.com>
2018-10-08 15:10:46 +0700

    update assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouteModule } from './route.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
